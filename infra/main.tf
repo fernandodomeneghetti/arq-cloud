c3133896345f7f191741a40c5e179ec80464d411
fernandodomeneghetti <fernando.miranda@anchieta.br>
2026-02-28 14:33:33 -0300

    add project

---
+++ infra/main.tf
@@ -0,0 +1,49 @@
+# 1. Definição dos Providers
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "unianchieta-arq-cloud" # Nome único
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# 2. Grupo de Recursos - O "container" lógico
+resource "azurerm_resource_group" "aula_rg" {
+  name     = "rg-aula-terraform-cd"
+  location = "East US"
+}
+
+# 3. Plano de Serviço (F1 é a camada gratuita/Free)
+resource "azurerm_service_plan" "aula_plan" {
+  name                = "asp-aula-site"
+  resource_group_name = azurerm_resource_group.aula_rg.name
+  location            = azurerm_resource_group.aula_rg.location
+  os_type             = "Linux"
+  sku_name            = "F1" 
+}
+
+# 4. O Web App propriamente dito
+resource "azurerm_linux_web_app" "aula_webapp" {
+  name                = "webapp-unianchieta-arq-cloud" # Nome deve ser ÚNICO na Azure
+  resource_group_name = azurerm_resource_group.aula_rg.name
+  location            = azurerm_service_plan.aula_plan.location
+  service_plan_id     = azurerm_service_plan.aula_plan.id
+
+  site_config {
+    application_stack {
+      node_version = "18-lts"
+    }
+  }
+}
